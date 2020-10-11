1c75d5846f1efcb1ad97d03eb6080aca14653f06
Tristone Muhunde <30902077+muhundet@users.noreply.github.com>
2020-10-11 23:29:57 +0200

    current register recycler adapter

--- app/src/main/java/com/example/smartlogbook/models/RegisterEntryModel.java
+++ app/src/main/java/com/example/smartlogbook/models/RegisterEntryModel.java
@@ -1,10 +1,97 @@
 package com.example.smartlogbook.models;
 
-public class RegisterEntry {
+public class RegisterEntryModel {
     private String registerEntryId;
     private String employeeId;
+    private String employeeName;
+    private String employeeSurname;
+    private String employeeDepartment;
     private String date;
     private String time_in;
     private String time_out;
     private String status;
+
+    public RegisterEntryModel(String registerEntryId, String employeeId, String employeeName, String employeeSurname, String employeeDepartment, String date, String time_in, String time_out, String status) {
+        this.registerEntryId = registerEntryId;
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.employeeSurname = employeeSurname;
+        this.employeeDepartment = employeeDepartment;
+        this.date = date;
+        this.time_in = time_in;
+        this.time_out = time_out;
+        this.status = status;
+    }
+
+    public String getRegisterEntryId() {
+        return registerEntryId;
+    }
+
+    public void setRegisterEntryId(String registerEntryId) {
+        this.registerEntryId = registerEntryId;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getEmployeeSurname() {
+        return employeeSurname;
+    }
+
+    public void setEmployeeSurname(String employeeSurname) {
+        this.employeeSurname = employeeSurname;
+    }
+
+    public String getEmployeeDepartment() {
+        return employeeDepartment;
+    }
+
+    public void setEmployeeDepartment(String employeeDepartment) {
+        this.employeeDepartment = employeeDepartment;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime_in() {
+        return time_in;
+    }
+
+    public void setTime_in(String time_in) {
+        this.time_in = time_in;
+    }
+
+    public String getTime_out() {
+        return time_out;
+    }
+
+    public void setTime_out(String time_out) {
+        this.time_out = time_out;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- app/src/main/java/com/example/smartlogbook/ui/main/CurrentRegisterRecyclerAdapter.java
+++ app/src/main/java/com/example/smartlogbook/ui/main/CurrentRegisterRecyclerAdapter.java
@@ -1,4 +1,63 @@
 package com.example.smartlogbook.ui.main;
 
-public class CurrentRegisterRecyclerAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.smartlogbook.R;
+import com.example.smartlogbook.models.RegisterEntryModel;
+
+import java.util.ArrayList;
+
+public class CurrentRegisterRecyclerAdapter extends RecyclerView.Adapter<CurrentRegisterRecyclerAdapter.CurrentRegisterViewHolder> {
+
+    ArrayList<RegisterEntryModel> registerEntries;
+    @NonNull
+    @Override
+    public CurrentRegisterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        View itemView = LayoutInflater.from(context).inflate(R.layout.employee_list_item,parent,false);
+        return new CurrentRegisterViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CurrentRegisterViewHolder holder, int position) {
+        RegisterEntryModel registerEntryModel = registerEntries.get(position);
+        holder.bind(registerEntryModel);
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class CurrentRegisterViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvName, tvDepartment, tvTimeIn, tvTimeOut;
+
+        public CurrentRegisterViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tv_employee_name);
+            tvDepartment = itemView.findViewById(R.id.tv_employee_department);
+            tvTimeIn = itemView.findViewById(R.id.tv_time_in);
+            tvTimeOut = itemView.findViewById(R.id.tv_time_out);
+
+        }
+
+        public void bind(RegisterEntryModel registerEntryModel){
+            tvName.setText(registerEntryModel.getEmployeeId() +", " + registerEntryModel.getEmployeeName() +", " +  registerEntryModel.getEmployeeSurname());
+            tvDepartment.setText(registerEntryModel.getEmployeeDepartment());
+            tvTimeIn.setText(registerEntryModel.getTime_in());
+            tvTimeOut.setText(registerEntryModel.getTime_out());
+        }
+
+    }
 }
